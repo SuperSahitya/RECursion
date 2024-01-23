d935edb46a77787dbece6b82bdc1b3fa1f79d20c
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-01-23 12:32:29 +0530

    fixed the facebook icon

--- Components/Member.jsx
+++ Components/Member.jsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
 import style from "./Member.module.css";
-import { FaGithubSquare, FaLinkedin, FaPhoneSquareAlt } from "react-icons/fa";
+import { FaLinkedin, FaPhoneSquareAlt } from "react-icons/fa";
+import { FaSquareFacebook } from "react-icons/fa6";
 
 const Member = ({ memName, imageurl, role, github, linkedin, phone }) => {
   const copyToClipboard = (e) => {
@@ -21,7 +22,7 @@ const Member = ({ memName, imageurl, role, github, linkedin, phone }) => {
           <div id={style.links}>
             <div id={style.github}>
               <a href={github} target="_blank" rel="noopener noreferrer">
-                <FaGithubSquare className={style.icons} />
+                <FaSquareFacebook className={style.icons} />
               </a>
             </div>
             <div id={style.linkedin}>
